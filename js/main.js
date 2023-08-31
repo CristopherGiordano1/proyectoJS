630df90644a90abdbb1466ff3c8bd0102a0c9900
Cristopher Giordano <criss_giordano@hotmail.com>
2023-08-30 23:58:07 -0300

    agregué un ejercicio con fory else

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
 //Ejemplos de variables
 
-let kilogramosBolsa = prompt ("Ingrese kilos de la bolsa en gramos");
+/*let kilogramosBolsa = prompt ("Ingrese kilos de la bolsa en gramos");
 let kilogramosMascota = prompt ("Ingrese el peso de su mascota");
 let gramosMascota = (kilogramosMascota * 10);
 let alertaDia = kilogramosBolsa / gramosMascota;
 
 
 
-alert("Te va a llegar el alerta dento de " + alertaDia.toFixed(2) + " días");
+alert("Te va a llegar el alerta dento de " + alertaDia.toFixed(2) + " días");*/
 
+//ejemplo con for
+
+/*for(let i=1; 1<=3; i++){
+    
+let tiempoCoccion = prompt("Pusiste los fideos?")
+if  (tiempoCoccion === "si")(
+    alert ("Perfecto")
+)
+
+}*/
+
+//segundo ejemplo con for y else
+
+/*for(let i=1; 1<=21; i++){
+    let duracionCoccion = parseInt(prompt("Hace cuántos minutos pusiste los fideos?"))
+    if (duracionCoccion === 20){
+        alert ("Ya están listos")
+        break;
+    }
+        else (alert ("todavía no están"))
+
+}*/
