854fb9ec406c92b666a1ccd6a002ef4338fb7f5c
Cristopher Giordano <criss_giordano@hotmail.com>
2023-09-09 19:45:57 -0300

    agregué un if else y un while

--- js/simulador.js
+++ js/simulador.js
@@ -1,10 +1,28 @@
 alert("¡Bienvenido!"); 
 
-let tipoMascota = (prompt("De qué raza es tu mascota? (Perro o gato)"));
-    console.log("Mi mascota es " + tipoMascota);   
+let tipoMascota = prompt("De qué raza es tu mascota? (Perro o gato)");
+    
+    if (tipoMascota == "gato"){
+        alert("Mi mascota es un gato");
+        
+    }else if(tipoMascota == "perro"){
+        alert("Mi mascota es un perro");
+        
+    }
+
+    while(tipoMascota != "perro" && tipoMascota != "gato"){
+        alert("Ingrese nuevamente");
+        tipoMascota = (prompt("De qué raza es tu mascota? (Perro o gato)"));   
+    }
+    console.log("Mi mascota es " + tipoMascota);
 
 let pesoMascota = (prompt("¿Cuántos kilos pesa?"));
     console.log("Mi mascota pesa " + pesoMascota + " kilos");
+
+    while(pesoMascota  <= 0 && pesoMascota >= 100){
+        alert("Ingrese nuevamente");
+        pesoMascota = (prompt("¿Cuántos kilos pesa?"));   
+    }
 
 let gramosDia = parseInt(prompt("¿Cuántos gramos de alimento le das a tu mascota por día?"));
     console.log("Mi mascota come " + gramosDia + " gramos por día");
@@ -15,7 +33,10 @@ let pesoBolsa = parseFloat(prompt("¿De cuántos kilos es la bolsa de alimento b
 let frecuenciaCompra = (pesoBolsa / gramosDia);
     console.log("El alimento te va a durar " + parseInt(frecuenciaCompra) + " días");
 
-    alert("La bosa te va a durar " + parseInt(pesoBolsa / gramosDia) + " días")
+
+
+
+    alert("La bolsa te va a durar " + parseInt(pesoBolsa / gramosDia) + " días")
     
 
 
