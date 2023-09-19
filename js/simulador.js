925210409834b10b6017ad85a038b7dfc52192c2
Cristopher Giordano <criss_giordano@hotmail.com>
2023-09-19 14:18:39 -0300

    Ciclos

--- js/simulador.js
+++ js/simulador.js
@@ -19,21 +19,14 @@ let tipoMascota = prompt("De qué raza es tu mascota? (Perro o gato)");
 let pesoMascota = (prompt("¿Cuántos kilos pesa?"));
     console.log("Mi mascota pesa " + pesoMascota + " kilos");
 
-    while(pesoMascota  <= 0 && pesoMascota >= 100){
-        alert("Ingrese nuevamente");
-        pesoMascota = (prompt("¿Cuántos kilos pesa?"));   
-    }
-
 let gramosDia = parseInt(prompt("¿Cuántos gramos de alimento le das a tu mascota por día?"));
-    console.log("Mi mascota come " + gramosDia + " gramos por día");
+    console.log("Mi mascota come " + gramosDia + " gramos por día"); 
 
 let pesoBolsa = parseFloat(prompt("¿De cuántos kilos es la bolsa de alimento balanceado que compraste?"))*1000;
     console.log("Compré una bolsa de " + pesoBolsa + " gramos");
 
 let frecuenciaCompra = (pesoBolsa / gramosDia);
     console.log("El alimento te va a durar " + parseInt(frecuenciaCompra) + " días");
-
-
 
 
     alert("La bolsa te va a durar " + parseInt(pesoBolsa / gramosDia) + " días")
